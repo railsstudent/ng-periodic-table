727e5c21f460acef2ea6a8b62f21e26d4d33dd92
Connie Leung <cleung334@gmail.com>
2018-03-17 23:45:22 +0800

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SelectionBarComponent } from './selection-bar/selection-bar.component';
+import { PeriodicTableComponent } from './periodic-table/periodic-table.component';
+import { AtomComponent } from './atom/atom.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectionBarComponent,
+    PeriodicTableComponent,
+    AtomComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/atom/atom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atom',
+  templateUrl: './atom.component.html',
+  styleUrls: ['./atom.component.scss']
+})
+export class AtomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
