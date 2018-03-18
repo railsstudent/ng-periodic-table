fbae8f5425315445dd12fb0af42fa81e5046933a
Connie Leung <cleung334@gmail.com>
2018-03-18 12:24:30 +0800

    Add dummy atom data

--- src/app/atom/atom.component.ts
+++ src/app/atom/atom.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-atom',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./atom.component.scss']
 })
 export class AtomComponent implements OnInit {
+
+  @Input()
+  data: any;
 
   constructor() { }
 
--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeriodicTableComponent implements OnInit {
 
+  rowHeader: { index: number, description: string }[];
+  atoms: number[];
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.rowHeader = Array(18).fill(1).map((v, i) => ({
+      index: i+1,
+      description: i === 14 ? 'Pnictogens':  (i === 15? 'Chalcogens' : (i === 16 ? 'Halogens': ''))
+    }));
 
+    this.atoms = Array(36).fill(1).map((v, i) => i+1);
+  }
 }
