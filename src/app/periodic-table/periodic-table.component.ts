341ec02d6d7470858ffa2693dcec9d8cfe4e63f4
Connie Leung <cleung334@gmail.com>
2018-12-01 15:29:36 +0800

    add husky and prettier-quick
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,226 +1,190 @@
 import {
-  Component,
-  OnInit,
-  Input,
-  OnChanges,
-  SimpleChanges,
-  ChangeDetectionStrategy,
-  Output,
-  EventEmitter,
-  ChangeDetectorRef
-} from "@angular/core";
-import { Atom, HighlightState, MatterType } from "../shared";
-import { get, assign } from "lodash-es";
-import { HttpClient } from "@angular/common/http";
-import { Observable, Subject, of } from "rxjs";
-import { map, takeUntil, tap, catchError } from "rxjs/operators";
+    Component,
+    OnInit,
+    Input,
+    OnChanges,
+    SimpleChanges,
+    ChangeDetectionStrategy,
+    Output,
+    EventEmitter,
+    ChangeDetectorRef,
+} from '@angular/core';
+import { Atom, HighlightState, MatterType } from '../shared';
+import { get, assign } from 'lodash-es';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, of, combineLatest } from 'rxjs';
+import { map, takeUntil, tap, catchError } from 'rxjs/operators';
 
 const MAX_ROW_INDEX = 7;
 const MAX_COL_INDEX = 18;
 const DESCRIPTION = {
-  number: "Atomic",
-  symbol: "SYM",
-  name: "Name",
-  atomic_mass: "Weight"
+    number: 'Atomic',
+    symbol: 'SYM',
+    name: 'Name',
+    atomic_mass: 'Weight',
 };
 const LANT_ATOM_GROUP = {
-  number: "57-71",
-  category: "lanthanide",
-  symbol: "",
-  name: "",
-  atomic_mass: null
+    number: '57-71',
+    category: 'lanthanide',
+    symbol: '',
+    name: '',
+    atomic_mass: null,
 };
 const ACT_ATOM_GROUP = {
-  number: "89-103",
-  category: "actinide",
-  symbol: "",
-  name: "",
-  atomic_mass: null
+    number: '89-103',
+    category: 'actinide',
+    symbol: '',
+    name: '',
+    atomic_mass: null,
 };
 
 @Component({
-  selector: "app-periodic-table",
-  templateUrl: "./periodic-table.component.html",
-  styleUrls: ["./periodic-table.component.scss"],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-periodic-table',
+    templateUrl: './periodic-table.component.html',
+    styleUrls: ['./periodic-table.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PeriodicTableComponent implements OnInit, OnChanges {
-  @Input()
-  selectedMetal: any;
-  @Input()
-  selectAllMetals: boolean;
-  @Input()
-  selectAllNonmetals: boolean;
+    @Input()
+    selectedMetal: any;
+    @Input()
+    selectAllMetals: boolean;
+    @Input()
+    selectAllNonmetals: boolean;
 
-  @Output()
-  currentAtomCategory: EventEmitter<string> = new EventEmitter<string>();
+    @Output()
+    currentAtomCategory: EventEmitter<string> = new EventEmitter<string>();
 
-  description = DESCRIPTION;
-  lantAtomGroup = LANT_ATOM_GROUP;
-  actinideAtomGroup = ACT_ATOM_GROUP;
+    description = DESCRIPTION;
+    lantAtomGroup = LANT_ATOM_GROUP;
+    actinideAtomGroup = ACT_ATOM_GROUP;
 
-  colHeader: { index: number; description: string; selected: boolean }[];
-  rowHeader: { index: number; className: string; selected: boolean }[];
-  // atoms$: Observable<Atom[]>;
-  unsubscribe$ = new Subject<void>();
-  atoms: Atom[];
-  matterClass: MatterType;
-  metalClass: HighlightState;
-  allMetals: boolean;
-  allNonmetals: boolean;
-  atomDetails: boolean;
-  currentAtom: Atom;
-  currentRowHeader: number;
-  currentColHeader: number;
+    colHeader: { index: number; description: string; selected: boolean }[];
+    rowHeader: { index: number; className: string; selected: boolean }[];
+    // atoms$: Observable<Atom[]>;
+    unsubscribe$ = new Subject<void>();
+    atoms: Atom[];
+    matterClass: MatterType;
+    metalClass: HighlightState;
+    allMetals: boolean;
+    allNonmetals: boolean;
+    atomDetails: boolean;
+    currentAtom: Atom;
+    currentRowHeader: number;
+    currentColHeader: number;
 
-  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
-    this.colHeader = Array(MAX_COL_INDEX)
-      .fill(1)
-      .map((v, i) => ({
-        index: i + 1,
-        description:
-          i === 14
-            ? "Pnictogens"
-            : i === 15
-            ? "Chalcogens"
-            : i === 16
-            ? "Halogens"
-            : "",
-        selected: false
-      }));
+    constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
+        this.colHeader = Array(MAX_COL_INDEX)
+            .fill(1)
+            .map((v, i) => ({
+                index: i + 1,
+                description: i === 14 ? 'Pnictogens' : i === 15 ? 'Chalcogens' : i === 16 ? 'Halogens' : '',
+                selected: false,
+            }));
 
-    this.rowHeader = [
-      { index: 1, className: "one", selected: false },
-      { index: 2, className: "two", selected: false },
-      { index: 3, className: "three", selected: false },
-      { index: 4, className: "four", selected: false },
-      { index: 5, className: "fifth", selected: false },
-      { index: 6, className: "six", selected: false },
-      { index: 7, className: "seven", selected: false }
-    ];
+        this.rowHeader = [
+            { index: 1, className: 'one', selected: false },
+            { index: 2, className: 'two', selected: false },
+            { index: 3, className: 'three', selected: false },
+            { index: 4, className: 'four', selected: false },
+            { index: 5, className: 'fifth', selected: false },
+            { index: 6, className: 'six', selected: false },
+            { index: 7, className: 'seven', selected: false },
+        ];
 
-    this.matterClass = {
-      solid: false,
-      liquid: false,
-      gas: false,
-      unknown: false
-    };
+        this.matterClass = {
+            solid: false,
+            liquid: false,
+            gas: false,
+            unknown: false,
+        };
 
-    this.allMetals = false;
-    this.allNonmetals = false;
-    this.atomDetails = false;
-    this.currentAtom = null;
-    this.currentRowHeader = null;
-    this.currentColHeader = null;
-    this.atoms = null;
-  }
-
-  ngOnInit() {
-    // this.http.get<Atom[]>("./assets/periodic-table.json").subscribe(
-    //   (atoms: Atom[]) => {
-    //     this.atoms = atoms.map(a => assign({}, a, { blurry: false }));
-    //     this.cd.markForCheck();
-    //   },
-    //   err => {
-    //     console.error(err);
-    //     this.atoms = [];
-    //   }
-    // );
-
-    this.http
-      .get<Atom[]>("./assets/periodic-table.json")
-      .pipe(
-        map((atoms: Atom[]) =>
-          atoms.map(a => assign({}, a, { blurry: false }))
-        ),
-        catchError(() => of([])),
-        takeUntil(this.unsubscribe$)
-      )
-      .subscribe((atoms: Atom[]) => {
-        this.atoms = atoms;
-        this.cd.markForCheck();
-      });
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const {
-      selectedMetal = null,
-      selectAllMetals = null,
-      selectAllNonmetals = null
-    } = changes;
-    this.metalClass = get(selectedMetal, "currentValue", null);
-    this.allMetals = get(selectAllMetals, "currentValue", false);
-    this.allNonmetals = get(selectAllNonmetals, "currentValue", false);
-  }
-
-  blurRowAtoms({ rowNum, blurry }) {
-    this.atoms = this.atoms.map(atom =>
-      rowNum === atom.ypos ||
-      (rowNum === 6 && atom.ypos === 8) ||
-      (rowNum === 7 && atom.ypos === 9)
-        ? atom
-        : assign({}, atom, { blurry })
-    );
-  }
-
-  blurColAtoms({ colNum, blurry }) {
-    this.atoms = this.atoms.map(atom =>
-      colNum === atom.xpos && atom.ypos !== 8 && atom.ypos !== 9
-        ? atom
-        : assign({}, atom, { blurry })
-    );
-  }
-
-  updateRowHeaderSelected(rowNum: number, selected: boolean) {
-    this.unselectAllHeaders();
-    this.currentRowHeader = selected ? rowNum : null;
-    this.rowHeader[rowNum - 1].selected = selected;
-    this.blurRowAtoms({ rowNum, blurry: selected });
-  }
-
-  updateColHeaderSelected(colNum: number, selected: boolean) {
-    this.unselectAllHeaders();
-    this.currentColHeader = selected ? colNum : null;
-    this.colHeader[colNum - 1].selected = selected;
-    this.blurColAtoms({ colNum, blurry: selected });
-  }
-
-  unselectAllHeaders() {
-    this.rowHeader.forEach(r => (r.selected = false));
-    this.colHeader.forEach(c => (c.selected = false));
-  }
-
-  showAtomDetails(atomNumber: number) {
-    this.atomDetails = atomNumber !== null && typeof atomNumber !== "undefined";
-    this.rowHeader.forEach((r, index) => {
-      if (
-        r &&
-        r.selected &&
-        (!this.currentRowHeader || index !== this.currentRowHeader - 1)
-      ) {
-        r.selected = false;
-      }
-    });
-    this.colHeader.forEach((c, index) => {
-      if (
-        c &&
-        c.selected &&
-        (!this.currentColHeader || index !== this.currentColHeader - 1)
-      ) {
-        c.selected = false;
-      }
-    });
-    if (atomNumber) {
-      this.currentAtom = this.atoms.find(a => a.number === atomNumber);
-
-      const { xpos, ypos } = this.currentAtom;
-      if (ypos > MAX_ROW_INDEX) {
-        this.rowHeader[ypos - 2 - 1].selected = true;
-      } else {
-        this.rowHeader[ypos - 1].selected = true;
-        this.colHeader[xpos - 1].selected = true;
-      }
-      this.currentAtomCategory.emit(get(this.currentAtom, "category", null));
+        this.allMetals = false;
+        this.allNonmetals = false;
+        this.atomDetails = false;
+        this.currentAtom = null;
+        this.currentRowHeader = null;
+        this.currentColHeader = null;
+        this.atoms = null;
     }
-  }
+
+    ngOnInit() {
+        this.http
+            .get<Atom[]>('./assets/periodic-table.json')
+            .pipe(
+                map((atoms: Atom[]) => atoms.map(a => assign({}, a, { blurry: false }))),
+                catchError(() => of([])),
+                takeUntil(this.unsubscribe$)
+            )
+            .subscribe((atoms: Atom[]) => {
+                this.atoms = atoms;
+                this.cd.markForCheck();
+            });
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        const { selectedMetal = null, selectAllMetals = null, selectAllNonmetals = null } = changes;
+        this.metalClass = get(selectedMetal, 'currentValue', null);
+        this.allMetals = get(selectAllMetals, 'currentValue', false);
+        this.allNonmetals = get(selectAllNonmetals, 'currentValue', false);
+    }
+
+    blurRowAtoms({ rowNum, blurry }) {
+        this.atoms = this.atoms.map(atom =>
+            rowNum === atom.ypos || (rowNum === 6 && atom.ypos === 8) || (rowNum === 7 && atom.ypos === 9)
+                ? atom
+                : assign({}, atom, { blurry })
+        );
+    }
+
+    blurColAtoms({ colNum, blurry }) {
+        this.atoms = this.atoms.map(atom =>
+            colNum === atom.xpos && atom.ypos !== 8 && atom.ypos !== 9 ? atom : assign({}, atom, { blurry })
+        );
+    }
+
+    updateRowHeaderSelected(rowNum: number, selected: boolean) {
+        this.unselectAllHeaders();
+        this.currentRowHeader = selected ? rowNum : null;
+        this.rowHeader[rowNum - 1].selected = selected;
+        this.blurRowAtoms({ rowNum, blurry: selected });
+    }
+
+    updateColHeaderSelected(colNum: number, selected: boolean) {
+        this.unselectAllHeaders();
+        this.currentColHeader = selected ? colNum : null;
+        this.colHeader[colNum - 1].selected = selected;
+        this.blurColAtoms({ colNum, blurry: selected });
+    }
+
+    unselectAllHeaders() {
+        this.rowHeader.forEach(r => (r.selected = false));
+        this.colHeader.forEach(c => (c.selected = false));
+    }
+
+    showAtomDetails(atomNumber: number) {
+        this.atomDetails = atomNumber !== null && typeof atomNumber !== 'undefined';
+        this.rowHeader.forEach((r, index) => {
+            if (r && r.selected && (!this.currentRowHeader || index !== this.currentRowHeader - 1)) {
+                r.selected = false;
+            }
+        });
+        this.colHeader.forEach((c, index) => {
+            if (c && c.selected && (!this.currentColHeader || index !== this.currentColHeader - 1)) {
+                c.selected = false;
+            }
+        });
+        if (atomNumber) {
+            this.currentAtom = this.atoms.find(a => a.number === atomNumber);
+
+            const { xpos, ypos } = this.currentAtom;
+            if (ypos > MAX_ROW_INDEX) {
+                this.rowHeader[ypos - 2 - 1].selected = true;
+            } else {
+                this.rowHeader[ypos - 1].selected = true;
+                this.colHeader[xpos - 1].selected = true;
+            }
+            this.currentAtomCategory.emit(get(this.currentAtom, 'category', null));
+        }
+    }
 }
--- src/app/selection-bar/selection-bar.component.ts
+++ src/app/selection-bar/selection-bar.component.ts
@@ -1,128 +1,134 @@
-import { Component, OnInit, Output, EventEmitter, ChangeDetectionStrategy, Input ,
-  OnChanges, SimpleChanges } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    Output,
+    EventEmitter,
+    ChangeDetectionStrategy,
+    Input,
+    OnChanges,
+    SimpleChanges,
+} from '@angular/core';
 import { HighlightState } from '../shared/';
 import { get } from 'lodash-es';
 
 const CATEGORIES = [
-  'alkali',
-  'alkaline',
-  'lant',
-  'actinoid',
-  'transition',
-  'postTransition',
-  'metalloid',
-  'nonMetal',
-  'nobleGas'
+    'alkali',
+    'alkaline',
+    'lant',
+    'actinoid',
+    'transition',
+    'postTransition',
+    'metalloid',
+    'nonMetal',
+    'nobleGas',
 ];
 
 @Component({
-  selector: 'app-selection-bar',
-  templateUrl: './selection-bar.component.html',
-  styleUrls: ['./selection-bar.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-selection-bar',
+    templateUrl: './selection-bar.component.html',
+    styleUrls: ['./selection-bar.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectionBarComponent implements OnInit, OnChanges {
+    @Output()
+    highlightElement: EventEmitter<HighlightState> = new EventEmitter<HighlightState>();
+    @Output()
+    selectAllMetals: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output()
+    selectAllNonmetals: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  @Output()
-  highlightElement: EventEmitter<HighlightState> = new EventEmitter<HighlightState>();
-  @Output()
-  selectAllMetals: EventEmitter<boolean> = new EventEmitter<boolean>();
-  @Output()
-  selectAllNonmetals: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Input()
+    currentAtomCategory: string;
 
-  @Input()
-  currentAtomCategory: string;
+    highlightState: HighlightState;
+    grayButtonStyle: any = null;
+    prevAtomCategory: string;
 
-  highlightState: HighlightState;
-  grayButtonStyle: any = null;
-  prevAtomCategory: string;
-
-  constructor() {
-    this.resetHighlight();
-    this.resetGrayButtons();
-    this.prevAtomCategory = null;
-  }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const { currentAtomCategory = null } = changes;
-    //console.log(currentAtomCategory);
-    const currentCategory = get(currentAtomCategory, 'currentValue', null);
-    //console.log('currentCategory', currentCategory);
-    this.resetHighlight();
-    if (currentCategory) {
-      let prop = '';
-      switch (currentCategory) {
-        case 'alkali-metal':
-          prop = 'alkali';
-          break;
-        case 'alkaline-earth-metal':
-          prop = 'alkaline';
-          break;
-        case 'lanthanide' :
-          prop = 'lant';
-          break;
-        case 'actinide':
-          prop = 'actinoid';
-          break;
-        case 'transition-metal':
-          prop = 'transition';
-          break;
-        case 'post-transition-metal':
-          prop = 'postTransition';
-          break;
-        case 'metalloid':
-          prop = 'metalloid';
-          break;
-        case 'nonmetal':
-          prop = 'nonMetal';
-          break;
-        case 'noble-gas':
-          prop = 'nobleGas';
-          break;
-      }
-      if (prop) {
-        this.highlightState[prop] = true;
-      }
+    constructor() {
+        this.resetHighlight();
+        this.resetGrayButtons();
+        this.prevAtomCategory = null;
     }
-  }
 
-  resetHighlight() {
-    this.highlightState = {
-      alkali: false,
-      alkaline: false,
-      lant: false,
-      actinoid: false,
-      transition: false,
-      postTransition: false,
-      metalloid: false,
-      nonMetal: false,
-      nobleGas: false
-    };
-  }
+    ngOnInit() {}
 
-  resetGrayButtons() {
-    this.grayButtonStyle = CATEGORIES.reduce((acc, c) => {
-      acc[c] = false;
-      return acc;
-    }, {});
-  }
-
-  changeHighlightState(key: string, value: boolean) {
-    this.resetHighlight();
-    this.highlightState[key] = value;
-    this.highlightElement.emit(this.highlightState);
-
-    const hasTrueValue = Object.keys(this.highlightState).some(k => this.highlightState[k] === true);
-    if (hasTrueValue === true) {
-      this.grayButtonStyle = CATEGORIES.reduce((acc, c) => {
-        acc[c] = this.highlightState[c] !== true;
-        return acc;
-      }, {});
-    } else {
-      this.resetGrayButtons();
+    ngOnChanges(changes: SimpleChanges) {
+        const { currentAtomCategory = null } = changes;
+        // console.log(currentAtomCategory);
+        const currentCategory = get(currentAtomCategory, 'currentValue', null);
+        // console.log('currentCategory', currentCategory);
+        this.resetHighlight();
+        if (currentCategory) {
+            let prop = '';
+            switch (currentCategory) {
+                case 'alkali-metal':
+                    prop = 'alkali';
+                    break;
+                case 'alkaline-earth-metal':
+                    prop = 'alkaline';
+                    break;
+                case 'lanthanide':
+                    prop = 'lant';
+                    break;
+                case 'actinide':
+                    prop = 'actinoid';
+                    break;
+                case 'transition-metal':
+                    prop = 'transition';
+                    break;
+                case 'post-transition-metal':
+                    prop = 'postTransition';
+                    break;
+                case 'metalloid':
+                    prop = 'metalloid';
+                    break;
+                case 'nonmetal':
+                    prop = 'nonMetal';
+                    break;
+                case 'noble-gas':
+                    prop = 'nobleGas';
+                    break;
+            }
+            if (prop) {
+                this.highlightState[prop] = true;
+            }
+        }
     }
-  }
+
+    resetHighlight() {
+        this.highlightState = {
+            alkali: false,
+            alkaline: false,
+            lant: false,
+            actinoid: false,
+            transition: false,
+            postTransition: false,
+            metalloid: false,
+            nonMetal: false,
+            nobleGas: false,
+        };
+    }
+
+    resetGrayButtons() {
+        this.grayButtonStyle = CATEGORIES.reduce((acc, c) => {
+            acc[c] = false;
+            return acc;
+        }, {});
+    }
+
+    changeHighlightState(key: string, value: boolean) {
+        this.resetHighlight();
+        this.highlightState[key] = value;
+        this.highlightElement.emit(this.highlightState);
+
+        const hasTrueValue = Object.keys(this.highlightState).some(k => this.highlightState[k] === true);
+        if (hasTrueValue === true) {
+            this.grayButtonStyle = CATEGORIES.reduce((acc, c) => {
+                acc[c] = this.highlightState[c] !== true;
+                return acc;
+            }, {});
+        } else {
+            this.resetGrayButtons();
+        }
+    }
 }
